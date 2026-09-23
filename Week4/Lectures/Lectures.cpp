6f28d8f2b1c8fca918f121d47f1756dcea0f924f
Garrett Girod <girodg@hotmail.com>
2026-09-23 09:42:45 -0400

    writing CSV

--- Week4/Lectures/Lectures.cpp
+++ Week4/Lectures/Lectures.cpp
@@ -3,10 +3,47 @@
 #include "Day10.h"
 #include "Day11.h"
 #include "Input.h"
+#include <fstream>
 
 
 int main(int argc, char* args[])
 {
+	//3 basic File I/O Steps:
+	//1) open the file
+	//	where the file is? how you're opening it? did it succeed?
+	//2) write/read the file
+	//3) CLOSE the file
+	
+	std::string fileName = "2609.csv";
+	std::string path = "C:\\temp\\2609\\";
+	std::string finalPath = path + fileName;
+	char delimiter = '$';
+	
+	//1) open the file
+	//this will attempt to open the file
+	//it will NOT work if the path does not exists
+	//if the file does not exists, it will create it
+	//if the file does exists, it will overwrite the data
+	std::ofstream outFile(finalPath);
+	if (outFile.is_open())
+	{
+		//2) write to the file
+		// << - insertion operator
+		outFile << "Batman rules!" << delimiter << 5;
+		outFile << delimiter << 13.7 << delimiter << true;
+		outFile << "\n" << "Aquaman smells like fish.";
+	}
+	else
+	{
+		std::cout << finalPath << " could not be opened.\n";
+	}
+	//3) CLOSE the file
+	//files are SYSTEM resources
+	//the OS can have only a few thousand files open
+	//good practice: close the file ASAP
+	outFile.close();
+
+
     std::string hello = "Hello Week 4!";
     for (auto& ch : hello)
     {
